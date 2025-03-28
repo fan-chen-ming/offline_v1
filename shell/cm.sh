2f1b153a094a3ade434134f08b393906610631a5
陈凡 <727372850@qq.com>
2025-03-28 22:00:09 +0800

    add 0328 coding

--- shell/cm.sh
+++ shell/cm.sh
@@ -1,3 +1,4 @@
 cd /opt/soft/datax/job/chenming
 python3 gen_import_config.py -d xxx -t  xxx
 sh mysql_to_hdfs all
+ ./bin/seatunnel.sh --config /opt/soft/seatunnel-2.3.10/user_config/2207A/chenming/my.conf
